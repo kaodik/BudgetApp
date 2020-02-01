5093073627170788e1bd396b1155a2762a6d722c
trumblay steven <trumblays@yahoo.com>
2020-01-31 20:06:24 -0600

    first commit

---
+++ BudgeApplication/src/sample/Controller.java
@@ -0,0 +1,18 @@
+package sample;
+
+public class Controller {
+    public void editBtnClicked(){
+        System.out.println("edit was clicked");
+    }
+
+    public void saveBtnClicked(){
+        System.out.println("Save was clicked");
+    }
+
+    public void newGoalBtnClicked(){
+        System.out.println("New Goal was clicked");
+
+    }
+
+
+}
